403fa87a35293c238de4e65051773701252f9bd6
jieling.guan <jieling.guan@jimu.com>
2016-08-02 14:28:25 +0800

    bugfix

--- clienthooks.js
+++ clienthooks.js
@@ -2,7 +2,7 @@ module.exports = {
   'applypath-msg': [],
   'commit-msg': [],
   'pre-applypath': [],
-  'pre-commit': [],
+  'pre-commit': ['mvn-compile'],
   'pre-push': [],
   'pre-rebase': [],
   'prepare-commit-msg': []
--- scripts/install.js
+++ scripts/install.js
@@ -3,7 +3,7 @@
 const shell = require('shelljs');
 
 if (shell.test('-f', './.gitconfig')) {
-  shell.mv('./.gitconfig', './.gitconfig.bak');
+  shell.mv('./.gitconfig', './.gitconfig.bak', { clobber: false });
 }
 
 shell.cp('./node_modules/client-hooks/.gitconfig', './gitconfig');
@@ -11,7 +11,7 @@ shell.cp('./node_modules/client-hooks/.gitconfig', './gitconfig');
 shell.exec(`git config --local include.path "../.gitconfig"`);
 
 if (shell.test('-f', './clienthooks.js')) {
-  shell.mv('./clienthooks.js', './clienthooks.js.bak');
+  shell.mv('./clienthooks.js', './clienthooks.js.bak', { clobber: false });
 }
 
 shell.cp('./node_modules/clienthooks/clienthooks.js', 'clienthooks.js');
