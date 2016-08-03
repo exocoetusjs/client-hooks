e1b88611705063dfa24ffe705e138107e6aa8419
jieling.guan <jieling.guan@jimu.com>
2016-08-03 16:46:49 +0800

    bugfix

--- clienthooks.js
+++ clienthooks.js
@@ -2,7 +2,7 @@ module.exports = {
   'applypath-msg': [],
   'commit-msg': [],
   'pre-applypath': [],
-  'pre-commit': ['test', 'test1'],
+  'pre-commit': ['test', 'test1', 'test1'],
   'pre-push': [],
   'pre-rebase': [],
   'prepare-commit-msg': []
--- plugins/test/test.plugin.sh
+++ plugins/test/test.plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "test1"
+echo "test"
