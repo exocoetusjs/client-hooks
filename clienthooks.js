75a9d3f5eab5cb9a7503c3b4c68eb1155a1f6b1a
jieling.guan <jieling.guan@jimu.com>
2016-08-03 18:51:50 +0800

    add

--- clienthooks.js
+++ clienthooks.js
@@ -2,7 +2,7 @@ module.exports = {
   'applypath-msg': [],
   'commit-msg': [],
   'pre-applypath': [],
-  'pre-commit': ['test', 'test1', 'test1', 'mvn-compile'],
+  'pre-commit': ['test', 'test1', 'test2'],
   'pre-push': [],
   'pre-rebase': [],
   'prepare-commit-msg': []
