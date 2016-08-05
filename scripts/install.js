225c15ae793ebed4bb113e141ae92d3fc84828f3
jieling.guan <jieling.guan@jimu.com>
2016-08-05 16:11:20 +0800

    bugfix

--- scripts/install.js
+++ scripts/install.js
@@ -2,13 +2,19 @@
 
 const shell = require('shelljs');
 
+const gitconfigAddr = 'https://raw.githubusercontent.com/crux-wild/' +
+  'client-hooks/master/.gitconfig';
+
+const clienthooksConfigAddr = 'https://raw.githubusercontent.com/crux-wild/' +
+  'client-hooks/master/clienthooks.js';
+
 shell.config.fatal = true;
 
 if (shell.test('-f', './.gitconfig')) {
   shell.mv('./.gitconfig', './.gitconfig.bak');
 }
 
-shell.cp('./node_modules/client-hooks/.gitconfig', './.gitconfig');
+shell.exec(`curl -fsSL ${gitconfigAddr}`);
 
 shell.exec(`git config --local include.path "../.gitconfig"`);
 
@@ -16,4 +22,4 @@ if (shell.test('-f', './clienthooks.js')) {
   shell.mv('./clienthooks.js', './clienthooks.js.bak');
 }
 
-shell.cp('./node_modules/clienthooks/clienthooks.js', 'clienthooks.js');
+shell.exec(`curl -fsSL ${clienthooksConfigAddr}`);
