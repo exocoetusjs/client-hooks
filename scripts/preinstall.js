1f4c0f70df34a0788a38f48cdcf467f907e4ca13
jieling.guan <jieling.guan@jimu.com>
2016-08-09 15:56:16 +0800

    add

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
-var checkGitRepo = '[ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1';
+const checkGitRepo = '[ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1';
 
 process.chdir('../../');
 
-exec(checkGitRepo, function(error) {
+exec(checkGitRepo, (error) => {
   if (error) {
     process.stderr.write('current directory must be a `git` repo\n');
     process.exit(1);
