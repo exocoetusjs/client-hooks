8aa09152a9910dea63d7b762556afe475ff20859
crux.wild <jimubox.crux.wild@gmail.com>
2016-08-14 11:28:43 +0800

    add

--- clienthooks.js
+++ clienthooks.js
@@ -18,17 +18,6 @@ module.exports = {
    */
   'applypath-msg': [],
   /**
-   * ## COMMIT-MSG
-   *
-   * This hook is invoked by `git commit`, and can be bypassed with
-   * `--no-verify` option. It takes a single parameter, the name of the file
-   * that holds the proposed commit log message. Exiting with non-zero status
-   * causes the git commit to abort.
-   *
-   * [More Detail](https://git-scm.com/docs/githooks#commit-msg)
-  */
-  'commit-msg': [],
-  /**
    * ## PRE-APPLYPATCH
    *
    * This hook is invoked by `git am`. It takes no parameter, and is invoked
@@ -39,7 +28,16 @@ module.exports = {
    *
    * [More Detail](https://git-scm.com/docs/githooks#pre-applypatch)
    */
-  'pre-applypath': [],
+  'pre-applypatch': [],
+  /**
+   * ## POST-APPLYPATCH
+   *
+   * This hook is invoked by `git am`. It takes no parameter, and is invoked
+   * after the patch is applied and a commit is made.
+   *
+   * [More Detail](https://git-scm.com/docs/githooks#post-applypatch)
+   */
+  'post-applypatch': [],
   /**
    * ## PRE-COMMIT
    *
@@ -52,6 +50,72 @@ module.exports = {
   */
   'pre-commit': [],
   /**
+   * ## PREPARE-COMMIT-MSG
+   *
+   * This hook is invoked by git commit right after preparing the default log
+   * message, and before the editor is started.
+   *
+   * [More Detail](https://git-scm.com/docs/githooks#pre-commit-msg)
+  */
+  'prepare-commit-msg': [],
+  /**
+   * ## COMMIT-MSG
+   *
+   * This hook is invoked by `git commit`, and can be bypassed with
+   * `--no-verify` option. It takes a single parameter, the name of the file
+   * that holds the proposed commit log message. Exiting with non-zero status
+   * causes the git commit to abort.
+   *
+   * [More Detail](https://git-scm.com/docs/githooks#commit-msg)
+  */
+  'commit-msg': [],
+  /**
+   * ## POST-COMMIT
+   *
+   * This hook is invoked by `git commit`. It takes no parameter, and is invoked
+   * after a commit is made.
+   *
+   * [More Detail](https://git-scm.com/docs/githooks#post-commit)
+  */
+  'post-commit': [],
+  /**
+   * ## PRE-REBASE
+   *
+   * This hook is called by `git rebase` and can be used to prevent a branch from
+   * getting rebased. The hook may be called with one or two parameters. The
+   * first parameter is the upstream from which the series was forked. The second
+   * parameter is the branch being rebased, and is not set when rebasing the
+   * current branch.
+   *
+   * [More Detail](https://git-scm.com/docs/githooks#pre-rebase)
+  */
+  'pre-rebase': [],
+  /**
+   * ## POST-CHECKOUT
+   *
+   * This hook is invoked when a `git checkout` is run after having updated the
+   * worktree. The hook is given three parameters: the ref of the previous `HEAD`,
+   * the ref of the new `HEAD` (which may or may not have changed), and a flag
+   * indicating whether the checkout was a branch checkout (changing branches,
+   * flag=1) or a file checkout (retrieving a file from the index, flag=0). This
+   * hook cannot affect the outcome of `git checkout`.
+   *
+   * [More Detail](https://git-scm.com/docs/githooks#post-checkout)
+  */
+  'post-checkout': [],
+  /**
+   * ## POST-MERGE
+   *
+   * This hook is invoked by `git merge`, which happens when a `git pull` is done
+   * on a local repository. The hook takes a single parameter, a status flag
+   * specifying whether or not the merge being done was a squash merge. This hook
+   * cannot affect the outcome of git merge and is not executed, if the merge
+   * failed due to conflicts.
+   *
+   * [More Detail](https://git-scm.com/docs/githooks#post-merge)
+  */
+  'post-merge': [],
+  /**
    * ## PRE-PUSH
    *
    * This hook is called by git push and can be used to prevent a push from taking
@@ -63,24 +127,23 @@ module.exports = {
   */
   'pre-push': [],
   /**
-   * ## PRE-REBASE
+   * ## POST-REWRITE
    *
-   * This hook is called by `git rebase` and can be used to prevent a branch from
-   * getting rebased. The hook may be called with one or two parameters. The first
-   * parameter is the upstream from which the series was forked. The second
-   * parameter is the branch being rebased, and is not set when rebasing the
-   * current branch.
+   * This hook is invoked by commands that rewrite commits (`git commit --amend`,
+   * git-rebase; currently git-filter-branch does not call it!). Its first argument
+   * denotes the command it was invoked by: currently one of amend or rebase.
+   * Further command-dependent arguments may be passed in the future.
    *
-   * [More Detail](https://git-scm.com/docs/githooks#pre-rebase)
+   * [More Detail](https://git-scm.com/docs/githooks#post-rewrite)
   */
-  'pre-rebase': [],
+  'post-rewrite': [],
   /**
-   * ## PREPARE-COMMIT-MSG
+   * ## PRE-AUTO-GC
    *
-   * This hook is invoked by git commit right after preparing the default log
-   * message, and before the editor is started.
+   * This hook is invoked by `git gc --auto`. It takes no parameter, and exiting
+   * with non-zero status from this script causes the `git gc --auto` to abort.
    *
-   * [More Detail](https://git-scm.com/docs/githooks#pre-commit-msg)
+   * [More Detail](https://git-scm.com/docs/githooks#pre-auto-gc)
   */
-  'prepare-commit-msg': [],
+ 'pre-auto-gc': [],
 };
