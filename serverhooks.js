3b8db9ec1c7db0b80d14e33c92c9ef6977a4a049
jieling.guan <jieling.guan@jimu.com>
2016-08-19 18:23:18 +0800

    add

--- serverhooks.js
+++ serverhooks.js
@@ -7,4 +7,36 @@
 *                                                                              *
 *******************************************************************************/
 module.exports = {
+  /**
+   * ## UPDATE
+   *
+   * This hook is invoked by git-receive-pack on the remote repository, which
+   * happens when a `git push` is done on a local repository. Just before updating
+   * the ref on the remote repository, the update hook is invoked. Its exit status
+   * determines the success or failure of the ref update.
+   *
+   * [More detail](https://git-scm.com/docs/githooks#update)
+   */
+  'update': [],
+  /**
+   * ## POST-RECEIVE
+   *
+   * This hook is invoked by git-receive-pack on the remote repository, which
+   * happens when a git push is done on a local repository. It executes on the
+   * remote repository once after all the refs have been updated.
+   *
+   * [More detail](https://git-scm.com/docs/githooks#pre-receive)
+   */
+  'post-receive': [],
+
+  /**
+   * ## POST-UPADTE
+   *
+   * This hook is invoked by git-receive-pack on the remote repository, which
+   * happens when a git push is done on a local repository. It executes on the
+   * remote repository once after all the refs have been updated.
+   *
+   * [More detail](https://git-scm.com/docs/githooks#pre-update)
+   */
+  'post-update': [],
 };
